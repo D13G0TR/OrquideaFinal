bf803fb64459edefc2253ce9e0c9ec6578bd63f0
Laxante99 <claudio.tzapata@gmail.com>
2024-12-04 17:20:35 -0300

    actualizacion DB

--- orquideas.sql
+++ orquideas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-12-2024 a las 02:51:24
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 04-12-2024 a las 21:18:56
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,19 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `actividades`
---
-
-CREATE TABLE `actividades` (
-  `id` int(11) NOT NULL,
-  `run_usuario` varchar(12) NOT NULL,
-  `descripcion` text NOT NULL,
-  `fecha_hora` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `orquidea`
 --
 
@@ -47,15 +34,16 @@ CREATE TABLE `orquidea` (
   `temperatura` decimal(5,2) NOT NULL,
   `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp(),
   `estado` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `orquidea`
 --
 
 INSERT INTO `orquidea` (`id`, `nombre`, `humedad`, `temperatura`, `fecha_registro`, `estado`) VALUES
-(1, 'cactus', 0.00, 0.00, '2024-11-30 17:18:44', 'Esperando Riego'),
-(2, 'flor de chota', 0.00, 0.00, '2024-12-01 01:46:14', 'Esperando Riego');
+(1, 'cactus', '87.00', '22.42', '2024-11-30 17:18:44', 'Regando'),
+(2, 'orquidea', '84.00', '27.28', '2024-11-30 17:18:44', 'Regando'),
+(3, 'planta', '87.00', '23.49', '2024-12-01 16:35:19', 'Regando');
 
 -- --------------------------------------------------------
 
@@ -68,7 +56,7 @@ CREATE TABLE `programacion_riego` (
   `planta_id` int(11) NOT NULL,
   `fecha_riego` date NOT NULL,
   `hora_riego` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `programacion_riego`
@@ -76,7 +64,27 @@ CREATE TABLE `programacion_riego` (
 
 INSERT INTO `programacion_riego` (`id`, `planta_id`, `fecha_riego`, `hora_riego`) VALUES
 (1, 1, '2024-11-01', '08:00:00'),
-(2, 2, '2024-11-08', '08:00:00');
+(2, 2, '2024-11-08', '08:00:00'),
+(3, 3, '2024-12-01', '14:00:00'),
+(4, 1, '2024-12-04', '17:00:00'),
+(5, 2, '2024-12-04', '17:00:00'),
+(6, 3, '2024-12-04', '17:00:00'),
+(7, 1, '2024-12-04', '17:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblhistorial`
+--
+
+CREATE TABLE `tblhistorial` (
+  `id` int(11) NOT NULL,
+  `planta_id` int(11) DEFAULT NULL,
+  `fecha_hora` datetime DEFAULT NULL,
+  `tipo_actividad` varchar(255) DEFAULT NULL,
+  `valor_anterior` varchar(255) DEFAULT NULL,
+  `valor_nuevo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -91,7 +99,7 @@ CREATE TABLE `usuarios` (
   `apellido` varchar(50) NOT NULL,
   `nombre_usuario` varchar(50) NOT NULL,
   `contraseña` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -104,13 +112,6 @@ INSERT INTO `usuarios` (`run`, `digito_verificador`, `nombre`, `apellido`, `nomb
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `actividades`
---
-ALTER TABLE `actividades`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `run_usuario` (`run_usuario`);
 
 --
 -- Indices de la tabla `orquidea`
@@ -126,6 +127,13 @@ ALTER TABLE `programacion_riego`
   ADD KEY `planta_id` (`planta_id`);
 
 --
+-- Indices de la tabla `tblhistorial`
+--
+ALTER TABLE `tblhistorial`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `planta_id` (`planta_id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -137,38 +145,38 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `actividades`
---
-ALTER TABLE `actividades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `orquidea`
 --
 ALTER TABLE `orquidea`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `programacion_riego`
 --
 ALTER TABLE `programacion_riego`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `tblhistorial`
+--
+ALTER TABLE `tblhistorial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `actividades`
---
-ALTER TABLE `actividades`
-  ADD CONSTRAINT `actividades_ibfk_1` FOREIGN KEY (`run_usuario`) REFERENCES `usuarios` (`run`) ON DELETE CASCADE;
-
---
 -- Filtros para la tabla `programacion_riego`
 --
 ALTER TABLE `programacion_riego`
   ADD CONSTRAINT `programacion_riego_ibfk_1` FOREIGN KEY (`planta_id`) REFERENCES `orquidea` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `tblhistorial`
+--
+ALTER TABLE `tblhistorial`
+  ADD CONSTRAINT `tblhistorial_ibfk_1` FOREIGN KEY (`planta_id`) REFERENCES `orquidea` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
